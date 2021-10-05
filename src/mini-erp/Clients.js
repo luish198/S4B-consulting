17734261ddc6d32d35c08f83fa8cf061d48f5fa4
luish198 <luish198@hotmail.com>
2021-10-05 21:05:35 +0200

    changing redirect file to public folder

--- src/mini-erp/Clients.js
+++ src/mini-erp/Clients.js
@@ -18,8 +18,8 @@ export default function Clients() {
 
     useEffect(() => {
 
-        fetch("http://localhost:5000/users/checkSession", { credentials: 'include' })
-            //fetch("https://s4b-consulting-user-api.herokuapp.com/users/checkSession", { credentials: 'include' })
+        //fetch("http://localhost:5000/users/checkSession", { credentials: 'include' })
+            fetch("https://s4b-consulting-user-api.herokuapp.com/users/checkSession", { credentials: 'include' })
 
             .then(response => {
                 if (response.status !== 200) {
